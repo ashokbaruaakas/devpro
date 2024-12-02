55f2cd4c0b93ecbba535dca4963b8240ec69fabc
Ashok <ashokbaruaakas@gmail.com>
2024-12-02 11:55:51 +0600

    Updated ConfigurePint action with stubs inseated of default file

--- app/Actions/ConfigurePint.php
+++ app/Actions/ConfigurePint.php
@@ -12,50 +12,42 @@ use function Laravel\Prompts\info;
 
 final class ConfigurePint implements Invokable
 {
-    private const systemConfigurationDir = '.devpro/default';
-
-    private const configurationFile = 'pint.json';
-
-    private string $applicationBasePath;
-
-    private string $currentWorkingDirectory;
+    private string $cwd;
 
     public function __construct()
     {
-        $this->applicationBasePath = base_path();
-        $this->currentWorkingDirectory = getcwd();
+        $this->cwd = getcwd();
     }
 
     public function __invoke(): void
     {
-        if (! File::exists("{$this->currentWorkingDirectory}/vendor/bin/pint")) {
-            $this->print('Pint not installed, installing it via composer...');
+        if (! File::exists("{$this->cwd}/vendor/bin/pint")) {
+            $this->consolePrint('Pint not installed, installing it via composer...');
             exec('composer require laravel/pint --dev');
         }
 
-        $targetedFilePath = "{$this->currentWorkingDirectory}/pint.json";
+        $targetedPath = "{$this->cwd}/pint.json";
 
-        if (File::exists($targetedFilePath) &&
+        if (File::exists($targetedPath) &&
             ! confirm('Do you want to overwrite the existing pint configuration file?', false)) {
-            $this->print('Pint configuration skipped.');
+            $this->consolePrint('Pint configuration skipped.');
 
             return;
         }
 
-        File::copy($this->pintConfigurationFilePath(), $targetedFilePath);
-        $this->print('Pint successfully configured.');
+        File::copy($this->pintStubPath(), $targetedPath);
+        $this->consolePrint('Pint successfully configured.');
     }
 
-    private function pintConfigurationFilePath(): string
+    private function pintStubPath(): string
     {
         return implode(DIRECTORY_SEPARATOR, [
-            $this->applicationBasePath,
-            self::systemConfigurationDir,
-            self::configurationFile,
+            config('devpro.stubs_path'),
+            'pint-json.stub',
         ]);
     }
 
-    private function print(string $message): void
+    private function consolePrint(string $message): void
     {
         if (! empty($message) && app()->runningInConsole()) {
             info($message);
