7b31b329c5bcc8ebf0ca0cf897d2f437756f4c00
Ashok <ashokbaruaakas@gmail.com>
2024-12-15 00:35:29 +0600

    Updated system scripts to standard

--- config/devpro.php
+++ config/devpro.php
@@ -6,11 +6,30 @@ return [
     'stubs_path' => base_path('stubs'),
 
     'scripts' => [
-        'dev' => 'npx concurrently -c "#c4b5fd,#fb7185,#fdba74" "php artisan queue:listen --tries=1 --timeout=0" "php artisan pail --timeout=0" "npm run dev" --names=QUEUE,LOGS,VITE',
+        'logs-clear' => 'find storage/logs -type f ! -name ".gitignore" -delete',
+
         'build' => 'npm run build',
-        'test' => 'php artisan test',
-        'format' => ['./vendor/bin/pint', 'npx prettier . --write', 'npm run lint'],
-        'analyse' => './vendor/bin/phpstan analyse --memory-limit=2G',
-        'log-clean' => 'find storage/logs -type f ! -name ".gitignore" -delete',
+        'dev' => 'npx concurrently -c "#c4b5fd,#fb7185,#fdba74" "php artisan queue:listen --tries=1 --timeout=0" "php artisan pail --timeout=0" "npm run dev" --names=QUEUE,LOGS,VITE',
+
+        'lint' => [
+            './vendor/bin/pint',
+            'npm run lint',
+        ],
+        'test:lint' => [
+            './vendor/bin/pint --test',
+        ],
+        'test:types' => [
+            './vendor/bin/phpstan analyse --memory-limit=1G',
+            'npm run test:types',
+        ],
+        'test:type-coverage' => './vendor/bin/pest --type-coverage --min=100 --memory-limit=1G',
+        'test:unit' => './vendor/bin/pest --parallel --ci --coverage --min=100.0',
+
+        'test' => [
+            '@test:lint',
+            '@test:types',
+            '@test:type-coverage',
+            '@test:unit',
+        ],
     ],
 ];
