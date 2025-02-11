8700555031a5063a79c47c092c1f5cf3073f541d
Ashok <ashokbaruaakas@gmail.com>
2025-02-11 11:53:14 +0600

    feat(constants): add LiteralValue class for constants

---
+++ app/Constants/LiteralValue.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Constants;
+
+final class LiteralValue
+{
+    public const TRUE = true;
+
+    public const FALSE = false;
+
+    public const NULL = null;
+
+    public const EMPTY_STRING = '';
+
+    public const EMPTY_ARRAY = [];
+
+    public const ZERO = 0;
+
+    public const PI = 3.14;
+}
