1201e83b9bfcc2a905353c9c32e20191abf22818
Ashok <ashokbaruaakas@gmail.com>
2025-02-11 11:51:09 +0600

    fix: improve absolute_path function for better handling

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,9 +2,15 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Str;
+
 if (! function_exists('absolute_path')) {
     function absolute_path(string $path = ''): string
     {
-        return getcwd().DIRECTORY_SEPARATOR.$path;
+        if (filled($path) && ! Str::startsWith(DIRECTORY_SEPARATOR, $path)) {
+            $path = DIRECTORY_SEPARATOR.$path;
+        }
+
+        return getcwd().$path;
     }
 }
